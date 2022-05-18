8309d79890a009145bc6a57beb6ee06be74ed25d
D.J. Figueroa <djfigs10@gmail.com>
2022-05-18 11:49:28 -0400

    Pretty lab gatekeeper

--- frontend/src/gatekeeper/lab-gatekeeper.tsx
+++ frontend/src/gatekeeper/lab-gatekeeper.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { TerpLinkScanner } from "./terplink-scanner";
+import "./lab-gatekeeper.css"
+import { Typography } from "@mui/material";
 
 export const LabGatekeeper: React.FC = ({ children }) => {
-    return <div>
+    return <div className="gk-root">
+      <Typography variant="h1" fontWeight={"bold"}>
+        Welcome to the XR Club Lab!
+      </Typography>
       <TerpLinkScanner eventcode="XE7M9EN" />
     </div>
 };
